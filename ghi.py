0fa001067e1be777410ccfe3da8aabe9717eb80f
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-24 15:41:31 +0900

    Update ghi.py

--- ghi.py
+++ ghi.py
@@ -61,17 +61,17 @@ for bd in range(1,16):
     #st.write(nd)
     lon = 0
     be = 0
+    for nu in range(0,tl):
+        kh = int(num_r[nu])
+        if kh > 50:
+            lon = lon + 1
+        else:
+            be = be + 1
     if tl != 0:
-        for nu in range(0,tl):
-            kh = int(num_r[nu])
-            if kh > 50:
-                lon = lon + 1
-            else:
-                be = be + 1
+        st.write("Số cầu thõa mãn là :",tl)
+        st.write("Tỉ lệ ra số Lớn là :",round((lon/tl)*100,2),"%")
+        st.write("Tỉ lệ ra số Bé là :",round((be/tl)*100,2),"%")
     else:
         st.write("Dữ liệu không có cầu này! Vui lòng chọn ngày cầu nhỏ hơn")
-    st.write("Số cầu thõa mãn là :",tl)
-    st.write("Tỉ lệ ra số Lớn là :",round((lon/tl)*100,2),"%")
-    st.write("Tỉ lệ ra số Bé là :",round((be/tl)*100,2),"%")
     num_r = []
 st.write(num_d)
