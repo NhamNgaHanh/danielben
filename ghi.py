cd196456d94ff328aa996f9547807fb28400000a
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-24 16:16:26 +0900

    Update ghi.py

--- ghi.py
+++ ghi.py
@@ -49,7 +49,7 @@ for bd in range(1,16):
                                 pl = i+m
                                 lits.append(df.iloc[pl, 1])
                             num_r.append(str(int(df.iloc[i-1, 1]))[-2:])
-                            #st.write(df.iloc[i, 0])
+                            st.write(df.iloc[i, 0])
                             #st.write(lits,)
                             #st.write(str(int(df.iloc[i-1, 1]))[-2:])
                     else:
