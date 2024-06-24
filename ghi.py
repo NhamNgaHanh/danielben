75b89a288cab2a86c341040e72f495049fa15ca7
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-24 15:39:39 +0900

    Update ghi.py

--- ghi.py
+++ ghi.py
@@ -70,8 +70,8 @@ for bd in range(1,16):
                 be = be + 1
     else:
         st.write("Dữ liệu không có cầu này! Vui lòng chọn ngày cầu nhỏ hơn")
-    num_r = []
     st.write("Số cầu thõa mãn là :",tl)
     st.write("Tỉ lệ ra số Lớn là :",round((lon/tl)*100,2),"%")
     st.write("Tỉ lệ ra số Bé là :",round((be/tl)*100,2),"%")
-    st.write(num_d)
+    num_r = []
+st.write(num_d)
