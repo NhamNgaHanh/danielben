487101e2d7cc3630619d96dc7499c0f76f82cf3e
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-24 15:17:19 +0900

    Update ghi.py

--- ghi.py
+++ ghi.py
@@ -55,21 +55,21 @@ for bd in range(1,16):
                         break
         #st.write("Các chỉ số hàng thỏa mãn điều kiện:")
         #st.write(str(num_r))
-            tl = len(num_r)
-            #st.write(tl)
-            st.subheader(f":red[Biên độ dao động của cầu = {bd}]")
-            #st.write(nd)
-            lon = 0
-            be = 0
-            if tl != 0:
-                for nu in range(0,tl):
-                    kh = int(num_r[nu])
-                    if kh > 50:
-                        lon = lon + 1
-                    else:
-                        be = be + 1
-                st.write("Tỉ lệ ra số Lớn là :",round((lon/tl)*100,2),"%")
-                st.write("Tỉ lệ ra số Bé là :",round((be/tl)*100,2),"%")
-            else:
-                st.write("Dữ liệu không có cầu này! Vui lòng chọn ngày cầu nhỏ hơn")
+        tl = len(num_r)
+        #st.write(tl)
+        st.subheader(f":red[Biên độ dao động của cầu = {bd}]")
+        #st.write(nd)
+        lon = 0
+        be = 0
+        if tl != 0:
+            for nu in range(0,tl):
+                kh = int(num_r[nu])
+                if kh > 50:
+                    lon = lon + 1
+                else:
+                    be = be + 1
+            st.write("Tỉ lệ ra số Lớn là :",round((lon/tl)*100,2),"%")
+            st.write("Tỉ lệ ra số Bé là :",round((be/tl)*100,2),"%")
+        else:
+            st.write("Dữ liệu không có cầu này! Vui lòng chọn ngày cầu nhỏ hơn")
 st.write(num_d)
