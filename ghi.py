b705c1532cd23b19619cb432c4d3dfdedf0baca1
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-24 15:22:02 +0900

    Update ghi.py

--- ghi.py
+++ ghi.py
@@ -62,7 +62,7 @@ for bd in range(1,16):
     lon = 0
     be = 0
     if tl != 0:
-        for nu in range(0,tl):
+        for nu in range(0,tl+1):
             kh = int(num_r[nu])
             if kh > 50:
                 lon = lon + 1
